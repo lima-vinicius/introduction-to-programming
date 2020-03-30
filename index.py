86ad3aec5df5c852689942deb827b61c559bec3c
viniciuslima-99 <vrpl@cin.ufpe.br>
2020-03-30 00:57:46 -0300

    Feature(reception-program) :  Added header

---
+++ index.py
@@ -0,0 +1,10 @@
+"""
+Federal University of Pernambuco - Brazil.
+Computer Center(CIn) (http://www.cin.ufpe.br)
+Information System Undergraduate
+IF968 - Programação 01 \n
+Author: Vinicius Rafael Pereira Lima 
+Email: vrpl@cin.ufpe.br
+Copyright(c) 2019 Vinícius Rafael Pereira de Lima\n
+"""
+
