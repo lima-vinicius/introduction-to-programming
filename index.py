daccf9d241b4d449957bf2c1fd0c80027265bec8
viniciuslima-99 <vrpl@cin.ufpe.br>
2020-03-30 01:16:21 -0300

    Feature(reception-program) : Added the functions to write the patient record in the users.txt file

--- index.py
+++ index.py
@@ -16,3 +16,37 @@ def datetime ():
     datahora = data.strftime("%d/%m/%Y %H:%M")
     return datahora
 
+#All the functions to writing
+def criacaoCadastro():
+    '''Function to create a patient record'''
+    print("<-------------------------------------------->")
+    cpf = int(input("CPF: "))
+    nome = input("Nome completo: ")
+    rg = input("RG: ")
+    idade = input("Idade: ")
+    estado = input("UF: ")
+    cidade = input("Cidade: ")
+    rua = input("Rua: ")
+    numero = input("Numero: ")
+    data = datetime()
+    dataAtualizacao = datetime()
+    dataConsulta = datetime()
+    print("Cadastro Realizado")
+    print("<-------------------------------------------->")
+    adicionarDicionario(nome, cpf, rg, idade, estado, cidade, rua, numero, data, dataAtualizacao, dataConsulta)
+
+def adicionarDicionario(nome, cpf, rg, idade, estado, cidade, rua, numero, data, dataAtualizacao, dataConsulta):
+    '''Function to add the patient record to a dictionary'''
+    dic[cpf] = ((nome), (rg), (idade), (estado), (cidade), (rua), (numero), (data), (dataAtualizacao), (dataConsulta))
+    escreverArquivo(dic)
+
+def escreverArquivo(dic):
+    '''Function to write the patient's record in a txt file'''
+    arquivo = open("users.txt", "w")
+    for chave in dic:
+        arquivo.write(str(chave))
+        arquivo.write("\n")
+        for elementos in dic[chave]:
+            arquivo.write(str(elementos))
+            arquivo.write("\n")
+    arquivo.close()
